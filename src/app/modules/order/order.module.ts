f0f852cc7761619106f3999d381313a3ac8bc262
balassit <blakelass@gmail.com>
2018-09-30 18:40:35 -0700

    wash settings

--- src/app/modules/order/order.module.ts
+++ src/app/modules/order/order.module.ts
@@ -4,13 +4,15 @@ import { OrderComponent } from '@app/modules/order/pages/order.component';
 import { OrderRoutingModule } from '@app/modules/order/order.routing';
 
 import { WashComponent } from '@app/modules/order/pages/wash/wash.component';
+import { ContactComponent } from '@app/modules/order/pages/contact/contact.component';
 
 import { SharedModule } from '@app/shared';
 
 @NgModule({
     declarations: [
         OrderComponent,
-        WashComponent
+        WashComponent,
+        ContactComponent
     ],
     imports: [
     SharedModule,
